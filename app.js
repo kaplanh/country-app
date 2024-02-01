469cd2ddb1f7cffc843061c6d17f7a15ddbfd4a6
kplnhsyn <kplnhsyn.49@gmail.com>
2024-02-01 13:24:52 +0100

    app.js & index.js & style.css updated

--- app.js
+++ app.js
@@ -1,35 +1,45 @@
 //? api linki :  https://restcountries.com/v3.1/all
 
-//? Tum sayfa yuklendiginde getData() fonk calistirarak  API'deki datayi indir ve  ulke isimlerini select'e bas
+//! Tum sayfa yuklendiginde getData() fonk calistirarak  API'deki datayi indir ve  ulke isimlerini select'e bas
 
 //? 1.yazim sekli
 
 window.addEventListener("load", () => {
-  getData();
+    getData();
 });
+
 //? 2.yazim sekli
 // window.onload = function () {
 //   getData();
 
 // }
+
+// !API den datalari ceken fonksiyon
+
 let ülkeler; //?datayi globale cekmek icin
 
 const getData = async () => {
-  try {
-    const URL = "https://restcountries.com/v3.1/all";
-    const res = await fetch(URL);
-    if (!res.ok) {
-      throw new Error();
-    }
-    const data = await res.json();
-    ülkeler = data; //?datayi countries e aktararak  globale cektim
-    console.log(ülkeler);
-    nameSelect(data);
+    try {
+        const URL = "https://restcountries.com/v3.1/all";
+        const res = await fetch(URL);
+        if (!res.ok) {
+            throw new Error();
+        }
+        const data = await res.json();
+        ülkeler = data; //?datayi countries e aktararak  globale cektim
+        // console.log(ülkeler);
+        nameSelect(data);
 
-    // console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
+        // console.log(data);
+    } catch (error) {
+        // console.log(error);
+        renderError(error);
+    }
+};
+
+const renderError = (error) => {
+    const cardSection = document.getElementById("cards");
+    cardSection.innerHTML = `<img src="./404.png" alt="" />`;
 };
 
 const select = document.querySelector(".form-select"); //?event dada kullanmak icin disarda tanimladim
@@ -37,56 +47,56 @@ const select = document.querySelector(".form-select"); //?event dada kullanmak i
 //? Card olarak DOM'a bas
 
 const nameSelect = (data) => {
-  ülkeler.forEach((element) => {
-    // console.log(element);
-    ülkeler=data; //?datayi countries e aktararak  globale cektim
-    ülkeler.sort((a, b) => {
-      const nameA = a.name.common.toUpperCase();
-      const nameB = b.name.common.toUpperCase();
-      if (nameA < nameB) {
-        return -1;
-      }
-      if (nameA > nameB) {
-        return 1;
-      }
-      return 0;
-    });
+    ülkeler.forEach((element) => {
+        // console.log(element);
+        ülkeler = data; //?datayi countries e aktararak  globale cektim
+        ülkeler.sort((a, b) => {
+            const nameA = a.name.common.toUpperCase();
+            const nameB = b.name.common.toUpperCase();
+            if (nameA < nameB) {
+                return -1;
+            }
+            if (nameA > nameB) {
+                return 1;
+            }
+            return 0;
+        });
 
-    // ! Yukarda ulkeleri siralayarak çalıştırılıyoruz
-    select.innerHTML += `<option value=${element.name.common}>${element.name.common}</option>`;
-  });
-  // domaYaz(data)
+        // ! Yukarda ulkeleri siralayarak çalıştırılıyoruz
+        select.innerHTML += `<option value=${element.name.common}>${element.name.common}</option>`;
+    });
+    // domaYaz(data)
 };
 
 select.addEventListener("change", (e) => {
-  const selected = e.currentTarget.value; //?currenttarget select 'i isaret eder
-  console.log(selected);
-  if (selected) {
-    // console.log(ülkeler);
-    const secilenUlke = ülkeler.filter((item) => {
-      return item.name.common === selected;
-    });
-    console.log(secilenUlke);
-    domaYaz(secilenUlke[0]);
-  }
+    const selected = e.currentTarget.value; //?currenttarget select 'i isaret eder
+    console.log(selected);
+    if (selected) {
+        // console.log(ülkeler);
+        const secilenUlke = ülkeler.filter((item) => {
+            return item.name.common === selected;
+        });
+        console.log(secilenUlke);
+        domaYaz(secilenUlke[0]);
+    }
 });
 
 const domaYaz = (y) => {
-  const {
-    flags: { png }, //?nested objectlerde destructuring
-    name: { common }, //?nested objectlerde destructuring
-    region,
-    capital,
-    languages,
-    currencies,
-    population,
-    borders,
-    maps: { googleMaps }, //?nested objectlerde destructuring
-  } = y;
+    const {
+        flags: { png }, //?nested objectlerde destructuring
+        name: { common }, //?nested objectlerde destructuring
+        region,
+        capital,
+        languages,
+        currencies,
+        population,
+        borders,
+        maps: { googleMaps }, //?nested objectlerde destructuring
+    } = y;
 
-  const cardSection = document.getElementById("cards");
-  cardSection.innerHTML = `<div class="card m-auto mt-4" style="width: 28rem">
-            <img src="${png}" class="card-img-top" alt="..." />
+    const cardSection = document.getElementById("cards");
+    cardSection.innerHTML = `<div class="card m-auto mt-1" style="width: 25rem;">
+            <img src="${png}" class="card-img-top " style="height:200px" alt="..." />
             <div class="card-body">
               <h5 class="card-title fs-2">${common}</h5>
             </div>
@@ -101,13 +111,13 @@ const domaYaz = (y) => {
             <li class="list-group-item">
               <i class="fas fa-lg fa-comments"></i>
               <span class="fw-bold"> Languages:</span>${Object.values(
-                languages
+                  languages
               )}
             </li>
             <li class="list-group-item">
               <i class="fas fa-lg fa-money-bill-wave"></i>
               <span class="fw-bold"> Currencies:</span> ${
-                Object.values(currencies)[0].name //?Object.values() metodunun ciktisi array dir
+                  Object.values(currencies)[0].name //?Object.values() metodunun ciktisi array dir
               }, ${Object.values(currencies)[0].symbol}
             </li>
             <li class="list-group-item">
